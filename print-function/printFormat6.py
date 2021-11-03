d7bce488f53a63ea24d092c2b23d8b24b5450b68
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-03 17:35:38 +0530

    Added a file.

---
+++ print-function/printFormat6.py
@@ -0,0 +1,11 @@
+x= 42
+y= 73
+
+if x<y:
+    z=122
+    print('x<y: x is {} and y is {}'.format(x,y))
+    print('Line 2')
+    print('Line 3')
+    print('Line 4')
+
+print(' z is {}'.format(z))#no space before .format

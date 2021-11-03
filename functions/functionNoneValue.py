f859d2a594521408eea8d63b04808209b028151f
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-03 21:18:44 +0530

    Added a file.

---
+++ functions/functionNoneValue.py
@@ -0,0 +1,8 @@
+def function(n=2): #default value
+    print(n)
+
+x= function (42)
+print(x)
+
+#returns none: it is a special keyword and a special value in python.
+#it means the absence of a value.

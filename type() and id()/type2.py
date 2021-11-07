f0959166ec43603f995116ef06cb78a21eb0a2ed
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-07 11:43:44 +0530

    Added a file.

---
+++ type() and id()/type2.py
@@ -0,0 +1,7 @@
+
+x=(1,2,3,4,5)
+print('x is {}'.format(x))
+print(type(x))
+
+#tuples are used to store multiple items in a single variables
+#tuple is one of the 4 built in data types in python used to store collections of data.

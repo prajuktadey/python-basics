fead4b8871155ab4d5406b642465aa72b1f8b885
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-07 12:03:44 +0530

    Added a file.

---
+++ type() and id()/id1.py
@@ -0,0 +1,7 @@
+x= (1,'two', 3.0, [4, 'four'], 5)
+y= (1, 'two',3.0,[4, 'four'], 5)
+print('x is {}'. format(x))
+print(id(x))
+print(id(y))
+
+#id function returns a unique identifier for each object

9b1fe722456ad4f432b0ec1fd4bd561f0fd9b355
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-07 20:00:42 +0530

    Added a file.

---
+++ type() and id()/id2.py
@@ -0,0 +1,7 @@
+x= (1,'two', 3.0, [4, 'four'], 5)
+y= (1, 'two',3.0,[4, 'four'], 5)
+print('x is {}'. format(x))
+print(id(x[1]))
+#return the same value because for x[1] only one object has been created
+print(id(y[1]))
+

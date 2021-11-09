34f58985d8682ecf9e13a1513624244247859aae
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-09 23:48:49 +0530

    Added a file.

---
+++ functions/functions2.py
@@ -0,0 +1,10 @@
+def main():
+    x=kitten()
+    print(x)
+    
+def kitten():
+    return('Meow.')
+    
+
+if __name__== '__main__': main()
+#the special variable name will return the name of the current module

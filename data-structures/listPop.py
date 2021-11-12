5fa1b186fedef04c8693992b104dd410e4d2325d
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-12 22:01:38 +0530

    Added a file.

---
+++ data-structures/listPop.py
@@ -0,0 +1,12 @@
+def main():
+    game = [ 'Rock', 'Paper', 'Scissors', 'Lizard', 'Spock' ]
+    i=game.index('Paper')
+    x=game.pop()#stack
+    print(x)
+    print_list(game)
+
+def print_list(o):
+    for i in o: print(i, end=' ', flush=True)
+    print()
+
+if __name__ == '__main__': main()

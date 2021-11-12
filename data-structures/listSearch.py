1b5531d05d2bcbd5f26eed6d9abed5de030cf6d0
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-12 21:56:05 +0530

    Added a file.

---
+++ data-structures/listSearch.py
@@ -0,0 +1,11 @@
+def main():
+    game = [ 'Rock', 'Paper', 'Scissors', 'Lizard', 'Spock' ]
+    i=game.index('Paper')
+    print(game[i])
+    print_list(game)
+
+def print_list(o):
+    for i in o: print(i, end=' ', flush=True)
+    print()
+
+if __name__ == '__main__': main()

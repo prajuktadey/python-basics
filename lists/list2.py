cdced31de22a48f9787ac0d48ac7cc4861cabba6
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-13 21:15:55 +0530

    Added a file.

---
+++ lists/list2.py
@@ -0,0 +1,13 @@
+def main():
+    seq = range(11)# 0 to 10
+    seq2=[x for x in seq if x% 3 !=0]
+    print_list(seq)
+    print_list(seq2)
+
+def print_list(o):
+    for x in o: print(x, end = ' ')
+    print()
+
+if __name__ == '__main__': main()
+
+# a list comprehension is a list created based on another list or iterator.

cc88c17a04810e18f1f77068f3946dad702653cb
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-13 21:08:14 +0530

    Added a file.

---
+++ sets/setsSortAnd.py
@@ -0,0 +1,12 @@
+def main():
+    a = set("We're gonna need a bigger boat.")
+    b = set("I'm sorry, Dave. I'm afraid I can't do that.")
+    print_set(a & b) #members that are both in set A and B
+    
+def print_set(o):
+    print('{', end = '')
+    for x in o: print(x, end = '')
+    print('}')
+
+if __name__ == '__main__': main()
+

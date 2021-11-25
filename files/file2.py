95b8fb8760972df067f1eddf99069c4a9c1804ae
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-26 00:37:22 +0530

    Added a file.

---
+++ files/file2.py
@@ -0,0 +1,10 @@
+def main():
+    infile = open('C:\\learning\\python-basics\\files\\lines.txt', 'rt')
+    outfile = open('C:\\learning\\python-basics\\files\\lines-copy.txt', 'wt')
+    for line in infile:
+        print(line.rstrip(), file=outfile)
+        print('.', end='', flush=True)
+    outfile.close()
+    print('\ndone.')
+
+if __name__ == '__main__': main()

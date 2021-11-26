fe245ccaabe445c806b577693ce5466661164926
Prajukta Dey <theprajuktadey@gmail.com>
2021-11-26 08:45:05 +0530

    Added a file.

---
+++ numeric functions/types.py
@@ -0,0 +1,14 @@
+x = '47'
+y = int(x)
+
+print(f'x is {type(x)}')
+print(f'x is {x}')
+print(f'y is {type(y)}')
+print(f'y is {y}')
+
+
+#output
+#x is <class 'str'>
+#x is 47
+#y is <class 'int'>
+#y is 47
